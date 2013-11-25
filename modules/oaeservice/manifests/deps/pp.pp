41824f49adf283eae1a42b25e4f62ee31e49f9ff
Branden Visser <mrvisser@gmail.com>
2013-11-25 13:33:37 -0500

    Use tar.gz for pdf2html binary package

--- modules/oaeservice/manifests/deps/pp.pp
+++ modules/oaeservice/manifests/deps/pp.pp
@@ -16,10 +16,10 @@ class oaeservice::deps::pp {
   # Download and unpack pdf2html archive
   archive { 'pdf2html':
     ensure        => present,
-    url           => 'https://s3.amazonaws.com/oae-releases/pdf2html/pdf2html-0.10-bin.tar',
+    url           => 'https://s3.amazonaws.com/oae-releases/pdf2html/pdf2html-0.10-bin.tar.gz',
     target        => '/',
-    digest_string => 'dc2899f18fe6d14ac8a6c05dbb047df5',
+    digest_string => 'ba033d8576291be5d671cc694b639836',
     digest_type   => 'md5',
-    extension     => 'tar'
+    extension     => 'tar.gz'
   }
 }
