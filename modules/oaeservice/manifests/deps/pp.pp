8537fc0fccab5dbe976029aac446ebf1d02e442e
Branden Visser <mrvisser@gmail.com>
2013-11-25 13:17:44 -0500

    Use pre-compiled archive for pdf2html

--- modules/oaeservice/manifests/deps/pp.pp
+++ modules/oaeservice/manifests/deps/pp.pp
@@ -9,8 +9,17 @@ class oaeservice::deps::pp {
     'pdftk',
     'chrpath',
     'libfontconfig1-dev',
-    'fonts-international',
-    'pdf2htmlex'
+    'fonts-international'
   ]
   package { $pp_packages: ensure => installed }
+
+  # Download and unpack pdf2html archive
+  archive { 'pdf2html':
+    ensure        => present,
+    url           => 'https://s3.amazonaws.com/oae-releases/pdf2html/pdf2html-0.10-bin.tar',
+    target        => '/',
+    digest_string => 'dc2899f18fe6d14ac8a6c05dbb047df5',
+    digest_type   => 'md5',
+    extension     => 'tar'
+  }
 }
